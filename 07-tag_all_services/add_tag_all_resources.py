668d9f95edf7928743a9eacec4d38ebd2129d4ef
Rendell Arruda <rendellarruda3@gmail.com>
2024-02-18 15:32:08 -0300

    fix:resolve arn fora do padrao

--- 07-tag_all_services/add_tag_all_resources.py
+++ 07-tag_all_services/add_tag_all_resources.py
@@ -1,7 +1,11 @@
 import boto3
+# importa o modulo para regex
+import re
 
-# Cria uma sessão do Boto3 usando um perfil chamado 'default'. 
-# No caso de uso de aws sso será importante usar
+# Cria uma expressão regular para validar ARNs
+arn_pattern = re.compile(r'arn:aws:[a-z0-9\-]+:[a-z0-9\-]+:[0-9]+:[a-zA-Z0-9\-\/]+')
+
+# Cria uma sessão do Boto3 usando um perfil chamado 'default'. No caso de uso de aws sso será importante usar
 session = boto3.Session(profile_name='default')
 
 # Cria um client para o serviço AWS Resource Groups Tagging API. 
@@ -10,7 +14,7 @@ client = session.client('resourcegroupstaggingapi')
 
 # definir quais tags serao adicionadas 
 tag_key = 'map-migrated'
-tag_value = 'mig180224'
+tag_value = 'agoraVai'
 
 try:
     # Faz uma chamada à operação get_resources para obter informações sobre os recursos.
@@ -23,23 +27,25 @@ try:
         resource_arn = resource['ResourceARN']
         print(resource_arn)
         
-        # Obtém as tags do recurso. Se o recurso não tiver tags, retorna uma lista vazia.
-        tags = resource.get('Tags',[])                
-        
-        # Verifica se a tag 'map-migrated' já existe no recurso.
-        if any(tag['Key'] == tag_key for tag in tags):
-            print(f'O resource: {resource_arn} já possui a tag {tag_key}')
-            response = client.tag_resources(
-                ResourceARNList=[resource_arn],Tags={tag_key: tag_value})
+        #validar o arn com metodo match
+        if arn_pattern.match(resource_arn): 
+            # Obtém as tags do recurso. Se o recurso não tiver tags, retorna uma lista vazia.
+            tags = resource.get('Tags',[])                
             
-        else:
-            # Se a tag não existir, imprime uma mensagem indicando que o recurso será taggeado.
-            print(f'O resource; {resource_arn} será tagueado')
+            # Verifica se a tag 'map-migrated' já existe no recurso.
+            if any(tag['Key'] == tag_key for tag in tags):
+                print(f'O resource: {resource_arn} já possui a tag {tag_key}')
+                response = client.tag_resources(
+                    ResourceARNList=[resource_arn],Tags={tag_key: tag_value})
+                
+            else:
+                # Se a tag não existir, imprime uma mensagem indicando que o recurso será taggeado.
+                print(f'O resource; {resource_arn} será tagueado')
+                
+                # Chama a operação tag_resources para adicionar a tag ao recurso.
+                response = client.tag_resources(
+                    ResourceARNList=[resource_arn],Tags={tag_key: tag_value})
             
-            # Chama a operação tag_resources para adicionar a tag ao recurso.
-            response = client.tag_resources(
-                ResourceARNList=[resource_arn],Tags={tag_key: tag_value})
-           
 
 # Captura exceções e imprime detalhes em caso de erro.
 except Exception as e:
