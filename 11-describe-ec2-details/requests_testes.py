bc327248633ab88917c8ce6407c5eb0879622fe7
Rendell Arruda <rendellarruda3@gmail.com>
2024-06-02 19:59:21 -0300

    feat: push file to s3

--- 11-describe-ec2-details/requests_testes.py
+++ 11-describe-ec2-details/requests_testes.py
@@ -49,9 +49,9 @@ for reservation in response["Reservations"]:
 # SALVAR OS DADOS EM DIFERENTES FORMATOS
 ####
 # especificar o nome do arquivo de saida em jSON
-output_file = "ec2_details.json"
+json_file = "ec2_details.json"
 ## Salvar a saída do codigo no arquivo txt
-with open(output_file, "w") as f:
+with open(json_file, "w") as f:
     json.dump(instances_details, f, indent=4)
 ####
 
@@ -67,3 +67,19 @@ try:
             writer.writerow(data)
 except IOError:
     print("Erro de E/S")
+
+print(f"Detalhes das instâncias EC2 foram salvos nos arquivos {json_file} e {csv_file}")
+
+## enviar os dados para o S3
+# Enviar o arquivo CSV para um bucket S3
+s3_client = session.client("s3")
+bucket_name = "ilustredev-ec2-details-bucket"
+s3_key = "details/ec2_instances.csv"
+
+try:
+    s3_client.upload_file(csv_file, bucket_name, s3_key)
+    print(
+        f"Arquivo {csv_file} foi enviado para o bucket S3 {bucket_name} com a chave {s3_key}."
+    )
+except Exception as e:
+    print(f"Erro ao enviar o arquivo para o bucket S3: {e}")
